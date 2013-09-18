10a349241c032d0a966ed4d475a9cdec1e9648b3
Josue Ibarra <josue@elninja.com>
2013-09-17 18:22:12 -0700

    nuevos cambios

--- application/migrations/003_Create_megatronics_schema.php
+++ application/migrations/003_Create_megatronics_schema.php
@@ -4,11 +4,36 @@ class Migration_Create_megatronics_schema extends CI_Migration {
 
 	function up()
 	{
+		// Crear tabla de productos (products)
+
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'MEDIUMINT',
+				'constraint' => '8',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'sku' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '20',
+			),
+			'description' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+			),
+			'qty' => array(
+				'type' => 'INT',
+				'constraint' => '100',
+			)
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('products');
 
 	}
 
 	function down() 
 	{
-
+		// Destruir tabla de productos
+		$this->dbforge->drop_table('products');
 	}
 }
---
+++ application/models/products_m.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Products_m extends CI_Model
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+
+
+
+
+}
